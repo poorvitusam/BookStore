7b8a1346008656b192f08784b909342376367a90
poorvitusam <poorvitusam@gmail.com>
2018-04-16 00:58:42 -0400

    Basic Reduc React connect

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+	return[
+		{title: 'Head First Java'},
+		{title: 'Javascript Good parts'},
+		{title: 'Cormen'},
+		{title: 'Head First Design Patterns'},
+		{title: 'Outliers'}
+	]
+}
